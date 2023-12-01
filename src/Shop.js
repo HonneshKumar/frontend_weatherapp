5e7e8bb400ef5f6dbc55ad5acbc41784738df650
HonneshKumar <151113518+HonneshKumar@users.noreply.github.com>
2023-12-01 14:37:23 +0530

    frontend_weatherapp

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+export default function Contact() {
+  return (
+    <div className="contact">
+     
+      <h1>Please contact for any queries</h1>
+      <h1>email:weather24@gmail.com</h1>
+      <h1>Phone no:+911234567891</h1>
+      
+    </div>
+  );
+}
